d8a4751e0d3be056d25b3729b023e8b6dd729220
Kanon-Hosen <freelancerkanon1@gmail.com>
2022-11-08 00:28:56 +0600

    services get

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,10 +9,66 @@ require('dotenv').config()
 const port = process.env.PORT || 4000;
 
 // * Mongobd connect:::::::::::::::::::::
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.we2oxi5.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 
 const dbConnect = () => {
-    
+    try {
+        client.connect();
+        console.log("Database Connected")
+    } catch (error) {
+        console.log(error.message)
+    }
 }
+dbConnect()
+
+const Services = client.db('ServicesDB').collection('services');
+
+app.post('/services', async (req, res) => {
+    try {
+        const body = req.body;
+
+        const service = await Services.insertOne(body);
+        
+        if (service.insertedId) {
+            res.send({
+                success: true,
+                message:"succesfully added"
+            })
+        }
+        else {
+            res.send({
+                success: false,
+                message:"Unsuccessfull"
+            })
+        }
+    } catch (error) {
+        res.send({
+            success: true,
+            message:error.message
+        })
+    }
+})
+
+// * service get::::::::::::::::::::::::::::
+app.get('/services', async (req, res) => {
+    try {
+        const cursor = Services.find({});
+        const services = await cursor.toArray();
+        res.send({
+            success: true,
+            message: "Success",
+            data:services
+        })
+    } catch (error) {
+        res.send({
+            success: false,
+            message: error.message,
+            data:[]
+        })
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Server started successfully');
